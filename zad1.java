68c01a268fb6d17037c3d88e8d3daa8cea939aa3
s24571 <91895392+s24571@users.noreply.github.com>
2022-03-23 00:35:06 +0100

    Create zad1.java

---
+++ zad1.java
@@ -0,0 +1,12 @@
+package com.company.zajecia1_8_03;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        double x = 9.5*4.5;
+        double y =2.5*3;
+        double z = 45.5-3.5;
+        double t = (x-y)/z;
+        System.out.println(t);
+    }
+}
